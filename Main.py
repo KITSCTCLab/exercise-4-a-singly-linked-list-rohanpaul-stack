505fd600c4df8158f0be786f73f488329930e9ab
rohanpaul-stack <110011527+rohanpaul-stack@users.noreply.github.com>
2022-08-29 21:15:46 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,47 +1,27 @@
 from typing import Optional
+
+
 class Node:
-    """
-    Provide necessary documentation
-    """
-    def __init__(self, data=None, next=None):
-        """
-        Provide necessary documentation
-        """
+    def _init_(self, data=None, next=None):
         self.data = data
         self.next = next
+
+
 from typing import Optional
+
+
 class Node:
-    """
-    This class describes Node objects to act as elements of the LinkedList
-    Attributes:
-        -> data - stored associated data
-        -> next - link to next node
-    """
-    def__init__(self, data=None, next=None):
-        """
-        Initialises the Node with given attributes
-        """
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
+
+
 class LinkedList:
-    """
-    This class implements LinkedList using Node objects
-    Methods:
-        -> insert_at_end - inserts node with data at the end of the list
-        -> status - displays all elements of the lisT
-    Attributes
-        -> self.head - contains first node of LinkedList, None if list empty
-    """
-    def__init__(self):
-        """
-        Initialize the head
-        """
+
+    def __init__(self):
         self.head = None
+
     def insert_at_end(self, data):
-        """
-        Insert node at end of the list
-        :param data: integer data that will be used to create a node
-        """
         new = Node(data, None)
         current = self.head
         if current is None:
@@ -50,38 +30,27 @@ class LinkedList:
             while current.next is not None:
                 current = current.next
             current.next = new
+
     def status(self):
-        """
-        It prints all the elements of list.
-        """
         elements = []
         current = self.head
         while current:
             elements.append(current.data)
             current = current.next
         print(elements)
+
+
 class Solution:
-    """
-    Class implementing functions to add numbers in a LinkedListT
-    
-    """
+
     def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
         LinkedList]:
-        """
-        :param first_list: Linkedlist with non-negative integers
-        :param second_list: Linkedlist with non-negative integers
-        :return: returns the sum as a linked list
-        """
         result = self.get_num(first_list) + self.get_num(second_list)
         sum_list = LinkedList()
         for digit in list(map(int, str(result)[::-1])):
             sum_list.insert_at_end(digit)
         return sum_list
+
     def get_num(self, l: Optional[LinkedList]) -> int:
-        """
-        :param l: LinkedList with non-negative integers
-        :return: returns digits of the list as a single integer
-        """
         curr = l.head
         if curr is None:
             return 0
@@ -90,6 +59,8 @@ class Solution:
             num = str(curr.data) + num
             curr = curr.next
         return int(num)
+
+
 # Do not edit the following code
 # Create an instance for LinkedList
 first_list = LinkedList()
